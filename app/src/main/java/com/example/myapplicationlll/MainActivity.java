2f92cf721ff74ab652e2856a94337ac8ace7bd3d
ababic <you@example.com>
2020-08-31 18:41:39 +0200

    init

---
+++ app/src/main/java/com/example/myapplicationlll/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.myapplicationlll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int ACTION_TAKE_PHOTO = 2;
+
+    private ImageView imageView;
+    private Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        imageView = (ImageView) findViewById(R.id.imageView1);
+        bitmap = null;
+        Button picSBtn = (Button) findViewById(R.id.btnIntendS);
+        setBtnListenerOrDisable(
+                picSBtn,
+                mTakePicSOnClickListener,
+                MediaStore.ACTION_IMAGE_CAPTURE
+        );
+    }
+
+    private void setBtnListenerOrDisable(Button btn, Button.OnClickListener onClickListener,
+            String intentName) {
+        if (isIntentAvailable(this, intentName)) {
+            btn.setOnClickListener(onClickListener);
+        } else {
+            btn.setText(
+                    getText(R.string.cannot).toString() + " " + btn.getText());
+            btn.setClickable(false);
+        }
+    }
+
+    public static boolean isIntentAvailable(Context context, String action) {
+        final PackageManager packageManager = context.getPackageManager();
+        final Intent intent = new Intent(action);
+        List<ResolveInfo> list =
+                packageManager.queryIntentActivities(intent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+
+    Button.OnClickListener mTakePicSOnClickListener =
+            new Button.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dispatchTakePictureIntent(ACTION_TAKE_PHOTO);
+                }
+            };
+
+    private void dispatchTakePictureIntent(int actionCode) {
+        Intent takeImageIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takeImageIntent, ACTION_TAKE_PHOTO);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case ACTION_TAKE_PHOTO: {
+                if (resultCode == RESULT_OK) {
+                    handleSmallCameraPhoto(data);
+                }
+                break;
+            }
+        }
+    }
+    private void handleSmallCameraPhoto(Intent intent) {
+        Bundle extras = intent.getExtras();
+        bitmap = (Bitmap) extras.get("data");
+        imageView.setImageBitmap(bitmap);
+        imageView.setVisibility(View.VISIBLE);
+    }
+}
